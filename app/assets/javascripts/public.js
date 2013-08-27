02ba66e80facaca94641f5cabd5d4369d7a4cb6f
regis bruggheman <regis@joufdesign.com>
2013-08-27 08:49:44 +0200

    bump

--- app/assets/javascripts/public.js
+++ app/assets/javascripts/public.js
@@ -1,6 +1,9 @@
 //= require jquery_ujs
 //= require respond.min.js
-jQuery(document).ready(function() { /*-----------------------------------------------------------------------------------*/
+
+jQuery(document).ready(function() { 
+
+/*-----------------------------------------------------------------------------------*/
 /*	Contacts
 /*-----------------------------------------------------------------------------------*/
 
@@ -46,7 +49,9 @@ jQuery(document).ready(function() { /*------------------------------------------
 
 
   /* Validation Form with AJAX on Submit */
-  jQuery('#submit').click(function() {
+
+  jQuery('#contact-form')
+  .on("ajax:beforeSend", function(evt, xhr, settings) {
     jQuery('span.error').fadeOut('slow');
     jQuery('span.valid').fadeOut('slow');
     jQuery('#thanks').hide();
@@ -56,39 +61,39 @@ jQuery(document).ready(function() { /*------------------------------------------
 
     var error = false;
 
-    var name = jQuery('#name').val();
+    var name = jQuery('#inquiry_name').val();
     if (name == "" || name == " ") {
-      jQuery('#name').after("<span class='error'></span>");
-      jQuery('#name').parent().find('.error').fadeIn('slow');
+      jQuery('#inquiry_name').after("<span class='error'></span>");
+      jQuery('#inquiry_name').parent().find('.error').fadeIn('slow');
       error = true;
     } else {
-      jQuery('#name').after("<span class='valid'></span>");
-      jQuery('#name').parent().find('.valid').fadeIn('slow');
+      jQuery('#inquiry_name').after("<span class='valid'></span>");
+      jQuery('#inquiry_name').parent().find('.valid').fadeIn('slow');
     }
 
     var checkEmail = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
-    var email = jQuery('#email').val();
+    var email = jQuery('#inquiry_email').val();
     if (email == "" || email == " ") {
-      jQuery('#email').after("<span class='error'></span>");
-      jQuery('#email').parent().find('.error').fadeIn('slow');
+      jQuery('#inquiry_email').after("<span class='error'></span>");
+      jQuery('#inquiry_email').parent().find('.error').fadeIn('slow');
       error = true;
     } else if (!checkEmail.test(email)) {
-      jQuery('#email').after("<span class='error'></span>");
-      jQuery('#email').parent().find('.error').fadeIn('slow');
+      jQuery('#inquiry_email').after("<span class='error'></span>");
+      jQuery('#inquiry_email').parent().find('.error').fadeIn('slow');
       error = true;
     } else {
-      jQuery('#email').after("<span class='valid'></span>");
-      jQuery('#email').parent().find('.valid').fadeIn('slow');
+      jQuery('#inquiry_email').after("<span class='valid'></span>");
+      jQuery('#inquiry_email').parent().find('.valid').fadeIn('slow');
     }
 
-    var message = jQuery('#message').val();
+    var message = jQuery('#inquiry_message').val();
     if (message == "" || message == " ") {
-      jQuery('#message').after("<span class='error'></span>");
-      jQuery('#message').parent().find('.error').fadeIn('slow');
+      jQuery('#inquiry_message').after("<span class='error'></span>");
+      jQuery('#inquiry_message').parent().find('.error').fadeIn('slow');
       error = true;
     } else {
-      jQuery('#message').after("<span class='valid'></span>");
-      jQuery('#message').parent().find('.valid').fadeIn('slow');
+      jQuery('#inquiry_message').after("<span class='valid'></span>");
+      jQuery('#inquiry_message').parent().find('.valid').fadeIn('slow');
     }
 
     if (error == true) {
@@ -98,43 +103,28 @@ jQuery(document).ready(function() { /*------------------------------------------
       }, 3000);
       return false;
     }
-
-    var data_string = jQuery('#contact-form').serialize();
-
-    jQuery.ajax({
-      type: "POST",
-      url: "/inquiries",
-      data: {
-        name: name,
-        email: email,
-        message: message
-      },
-      timeout: 6000,
-      error: function(request, error) {
-        if (error == "timeout") {
-          jQuery('#timedout').fadeIn('slow');
-          setTimeout(function() {
-            jQuery('#timedout').fadeOut('slow');
-          }, 3000);
-        } else {
-          jQuery('#state').fadeIn('slow');
-          jQuery("#state").html('The following error occured: ' + error + '');
-          setTimeout(function() {
-            jQuery('#state').fadeOut('slow');
-          }, 3000);
-        }
-      },
-      success: function() {
-        jQuery('span.valid').remove();
-        jQuery('#thanks').fadeIn('slow');
-        jQuery('input').val('');
-        jQuery('textarea').val('');
-        setTimeout(function() {
-          jQuery('#thanks').fadeOut('slow');
-        }, 3000);
-      }
-    });
-
-    return false;
-  });
+  })
+  .on("ajax:error", function(evt, xhr, status, error) {
+    if (error == "timeout") {
+      jQuery('#timedout').fadeIn('slow');
+      setTimeout(function() {
+        jQuery('#timedout').fadeOut('slow');
+      }, 3000);
+    } else {
+      jQuery('#state').fadeIn('slow');
+      jQuery("#state").html('The following error occured: ' + error + '');
+      setTimeout(function() {
+        jQuery('#state').fadeOut('slow');
+      }, 3000);
+    }
+	})
+	.on("ajax:success", function(evt, data, status, xhr) {
+    jQuery('span.valid').remove();
+    jQuery('#thanks').fadeIn('slow');
+    jQuery('input').val('');
+    jQuery('textarea').val('');
+    setTimeout(function() {
+      jQuery('#thanks').fadeOut('slow');
+    }, 3000);
+  })
 });
